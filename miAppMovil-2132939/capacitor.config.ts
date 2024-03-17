d4770e041ef97048fc485d1c7ebcb5fb47a46737
Jose Luis CG <142639379+JL1704@users.noreply.github.com>
2024-03-16 18:48:20 -0600

    Creacion del proyecto Ionic

---
+++ miAppMovil-2132939/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miAppMovil-2132939',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
